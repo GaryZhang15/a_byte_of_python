96f255ba1e5593a010604a1246f12c5b411ff52f
Gary Zhang <gary.zhang@nextestate.com>
2017-03-02 16:20:06 +0800

    day1 learning

---
+++ 001_str_format.py
@@ -0,0 +1,31 @@
+print('\n******Start Line******\n')
+
+age = 28
+name = 'Gary Zhang'
+
+print('{} was {} years old when he wrote this book'.format(name, age))
+
+print('Why is {} playing with that python?'.format(name))
+
+# decimal (.) precision of 3 for float '0.333'
+print('{0:.3f}'.format(1.0/3))
+# fill with underscores (_) with the text centered
+print('{0:_^11}'.format('good'))
+# keyword-based
+print('{name} wrote {book}'.format(name='Gary', book='The new book'))
+
+print('a', end='_')
+
+print('b')
+
+print('This is the first line\nThis is the second line')
+
+print('This is the first sentence. \
+This is the second sentence.')
+
+print(r'Newlines are indicated by \n')
+
+print('\n*******End Line*******\n')
+
+
+
---
+++ 002_var.py
@@ -0,0 +1,23 @@
+print('\n******Start Line******\n')
+
+i =5
+
+print(i)
+
+i = i + 1
+
+print(i)
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
+
+a = 'hello'; print(a)
+
+b = \
+5
+print(b)
+
+print('\n*******End Line*******\n')
+
+
